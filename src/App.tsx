69db684ffcf24e2cb880033387f4dc3e2b53cfc9
Colin Marshall <colin.marshall94@gmail.com>
2024-09-16 13:03:53 -0700

    add OpenAI to project; move strict mode to prevent hitting OpenAI rate limiter; test using OpenAI request in React

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,42 @@
+import React from "react";
+import { useEffect, useState } from "react";
+import OpenAI from "openai";
+
 function App() {
+  const API_KEY: string = import.meta.env.VITE_OPENAI_KEY as string;
+  const openai = new OpenAI({ apiKey: API_KEY, dangerouslyAllowBrowser: true });
+  const [text, setText] = useState("");
+
+  useEffect(() => {
+    const result = async function testAPI() {
+      try {
+        const completion = await openai.chat.completions.create({
+          model: "gpt-4o-mini",
+          messages: [
+            { role: "system", content: "You are a helpful assistant." },
+            {
+              role: "user",
+              content: "Write a haiku about recursion in programming.",
+            },
+          ],
+        });
+        console.log(completion.choices[0].message);
+        const aiResponse = await Promise.resolve(completion);
+        const content = aiResponse.choices[0].message.content;
+        if (content !== null) {
+          setText(content);
+        }
+      } catch (err) {
+        console.error(err);
+      }
+    };
+    result().catch((err: unknown) => { console.error(err); });
+  }, [openai.chat.completions, text]);
+
   return (
     <>
       <h1>AI Travel Planner</h1>
+      <p>OpenAI return message: {text}</p>
     </>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
 
-const rootElement = document.getElementById('root');
+const rootElement = document.getElementById("root");
 if (rootElement) {
-  createRoot(rootElement).render(
-    <StrictMode>
-      <App />
-    </StrictMode>,
-  );
+  createRoot(rootElement).render(<App />);
 } else {
-  console.error('Root element not found');
+  console.error("Root element not found");
 }
